71aa1259b61699d40c92a0913a52dbdc2125a671
Darek Bobak <deni+github@lithics.eu>
2016-12-20 14:12:59 +0100

    Przeniesienie starych translatorów i dodane aktualnych

---
+++ translators/@deni_Pandoc_citation.js
@@ -0,0 +1,37 @@
+﻿{
+	"translatorID":"04a25cf0-172c-73af-5eac-10c4a56d7a33",
+	"translatorType":3,
+	"label":"@Pandoc citation",
+	"creator":"Darek Bobak",
+	"target":"txt",
+	"minVersion":"1.0",
+	"maxVersion":"",
+	"priority":200,
+	"inRepository":false,
+	"configOptions": {
+			"getCollections": true
+	},
+	"displayOptions": {
+		"exportCharset":"UTF-8",
+		"exportNotes": false,
+		"exportFileData": false,
+		"useJournalAbbreviation": false,
+		"Keep updated": false
+		},
+	"lastUpdated":"2015-12-20 13:50:00"
+}
+
+// Cytowanie dla markdown (Pandoc)
+
+function doExport() {
+	Zotero.write(" [");
+	while(item = Zotero.nextItem()) {
+		var pagetext = "@" + Zotero.BetterBibTeX.keymanager.get(item, 'on-export').citekey;
+		Zotero.write(pagetext);
+		if (Zotero.nextItem()) {
+			Zotero.write("; ");
+		}
+	}
+	Zotero.write("] ");
+}
+
---
+++ translators/@deni_ResearchNotes_SMW_citation.js
@@ -0,0 +1,38 @@
+﻿{
+	"translatorID":"04a25cf0-172c-73af-5eac-10c4a56d7a32",
+	"translatorType":3,
+	"label":"@ResearchNotes_SMW_citation",
+	"creator":"Darek Bobak",
+	"target":"txt",
+	"minVersion":"1.0",
+	"maxVersion":"",
+	"priority":200,
+	"inRepository":false,
+	"configOptions": {
+			"getCollections": true
+	},
+	"displayOptions": {
+		"exportCharset":"UTF-8",
+		"exportNotes": false,
+		"exportFileData": false,
+		"useJournalAbbreviation": false,
+		"Keep updated": false
+		},
+	"lastUpdated":"2015-12-20 13:30:00"
+}
+
+// Cytowanie do MediaWiki dla rozszerzenia SMW Cite
+
+function doExport() {
+	var first = true;
+	while(item = Zotero.nextItem()) {
+	var pagetext = [
+		" [[CiteRef::",
+		Zotero.BetterBibTeX.keymanager.get(item, 'on-export').citekey,
+		"]] "
+		].join("");
+	Zotero.write(pagetext);
+	first = false;
+	}
+}
+
