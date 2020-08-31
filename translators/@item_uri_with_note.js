c1a42f02b50b3f0ca53b4d7c59a54ef0dc8713a4
Darek Bobak <deni@lithics.eu>
2020-08-31 13:23:58 +0200

    Update @item_uri_with_note.js

--- translators/@item_uri_with_note.js
+++ translators/@item_uri_with_note.js
@@ -1,7 +1,7 @@
 {
 	"translatorID": "a515a220-6fef-0111-1974-8025dfebbb8f",
-	"label": "@Item URI with note (TiddlyWiki)",
-	"creator": "Dariusz Bobak",
+	"label": "@Item URI with note",
+	"creator": "Emiliano Heyns, Dariusz Bobak",
 	"target": "txt",
 	"minVersion": "4.0.27",
 	"translatorType": 2,
@@ -18,7 +18,7 @@ function doExport() {
 	var item;
 	while(item = Zotero.nextItem()) {
 		const date = Zotero.BetterBibTeX.parseDate(item.date);
-		Zotero.write("<<fnote '");
+		Zotero.write("(");
 		if (item.creators[2]) {
 			Zotero.write(item.creators[0].lastName + ", " + item.creators[1].lastName + " et al., ");
 			}
@@ -28,7 +28,18 @@ function doExport() {
 		else {
 			Zotero.write(item.creators[0].lastName + ", ");
 			}
-		Zotero.write(date.year + ", " + item["title"] + " [[zotero|");
+		Zotero.write(date.year + ")");
+		Zotero.write(" <<fnote '");
+		if (item.creators[2]) {
+			Zotero.write(item.creators[0].lastName + ", " + item.creators[1].lastName + " et al., ");
+			}
+		else if (item.creators[1]) {
+			Zotero.write(item.creators[0].lastName + " and " + item.creators[1].lastName + ", ");
+			}
+		else {
+			Zotero.write(item.creators[0].lastName + ", ");
+			}
+		Zotero.write(date.year + ", " + item["title"] + ". [[zotero|");
 		Zotero.write(item.uri + "]] [ext[local|");
 		Zotero.write("zotero://select/items/");
 		var library_id = item.libraryID ? item.libraryID : 0;
